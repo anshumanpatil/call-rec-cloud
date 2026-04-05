48ef2c6705f02f92458d6baf59894e62e5371400
Anshuman <samradni9923897009@gmail.com>
2026-04-05 21:25:53 +0530

    flutter new

---
+++ android/app/src/main/kotlin/com/example/cll_upld/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.cll_upld
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- app/src/main/java/com/phoenix/callrecordingutil/helper/FileUploadObject.kt
+++
@@ -1,8 +0,0 @@
-package com.phoenix.callrecordingutil.helper
-
-object FileUploadObject {
-    lateinit var fileName: String
-    lateinit var fileFullPath: String
-    lateinit var fileDirectory: String
-    lateinit var fileNameOnServer: String
-}
